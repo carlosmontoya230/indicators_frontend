59351dcfe752d57b9e6f16c709e9cce4510df958
carlos mario montoya yepes <carlosyepes230@hotmail.com>
2025-06-04 18:20:47 -0500

    config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppRoutes from "./app/routes";
+import AppRoutes from "@/app/routes";
 
 import "./styles/index.css";
 
